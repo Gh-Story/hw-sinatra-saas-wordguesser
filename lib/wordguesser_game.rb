51db1894691e2c3f257ce1db9c84ad2a8fd8d08f
Gh-Story <hg820801350@icloud.com>
2021-10-16 18:24:32 -0700

    finished homework

--- lib/wordguesser_game.rb
+++ lib/wordguesser_game.rb
@@ -1,12 +1,47 @@
 class WordGuesserGame
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
 
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/wordguesser_game_spec.rb pass.
 
   # Get a word from remote "random word" service
 
-  def initialize(word)
+  def initialize(word=WordGuesserGame.get_random_word, guesses = '',wrong_guesses='')
     @word = word
+    @guesses = guesses
+    @wrong_guesses = wrong_guesses
+  end
+
+  #getter for attribute: word
+  def word
+    @word
+  end
+
+  #setter for attribute: word 
+  def word=(word)
+    @word=word
+  end
+
+  #getter for attribute: guesses
+  def guesses
+    @guesses
+  end
+    
+  #setter for attribute: guesses
+  def guesses=(guesses)
+    @guesses=guesses
+  end
+  
+  #getter for attribute: wrong_guesses
+  def wrong_guesses
+	  @wrong_guesses
+  end
+  
+  #setter for attribute: guesses
+  def wrong_guesses=(wrong_guesses)
+	  @wrong_guesses=wrong_guesses
   end
 
   # You can test it by installing irb via $ gem install irb
@@ -20,6 +55,37 @@ class WordGuesserGame
     Net::HTTP.new('randomword.saasbook.info').start { |http|
       return http.post(uri, "").body
     }
+  end
+
+
+  def guess(letter)
+    
+    letter = letter.downcase if(letter != nil)
+
+    raise ArgumentError.new("Not a valid letter") if (letter.nil? or /[^A-Za-z]/.match(letter) != nil or letter == '')
+    
+    return false if (@guesses.include? letter or @wrong_guesses.include? letter)
+    
+    if @word.include? letter
+      @guesses += letter
+      return true
+    else
+      @wrong_guesses += letter
+      return true
+    end
+  end
+    
+  def word_with_guesses
+    t=@guesses
+    return @word.gsub(/[^ #{t}]/, '-')
+  end
+
+  def check_win_or_lose
+    return :lose if (@wrong_guesses.length >= 7)
+  
+    return :win if (word_with_guesses == @word)
+    
+    return :play
   end
 
 end
